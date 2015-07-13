68053745ff3bdc296b41f6a85d49db540a1f0746
Matteson Raab <mraabhimself@gmail.com>
2015-07-13 16:30:07 -0700

    Add MIN / SID decode to conversions

--- src/util/convert.cpp
+++ src/util/convert.cpp
@@ -57,3 +57,59 @@ std::string bytesToHex(unsigned char* input, int size, bool byteswap) {
 
   return result;
 }
+
+std::string min1Decode(unsigned char input[3]) {
+    char buffer[6];
+    int32_t i, chunk1, chunk2, chunk3;
+    std::string str, result;
+
+    str = bytesToHex(input, 4, false);
+    i = strtoul(str.c_str(), nullptr, 16);
+
+    chunk1 = (i & 0xFFC000) >> 14;
+    chunk1 = ((chunk1 + 1) % 10) + (((((chunk1 % 100) / 10) + 1) % 10) * 10) + ((((chunk1 / 100) + 1) % 10) * 100);
+
+    chunk2 = ((i & 0x3C00) >> 10) % 10;
+
+    chunk3 = (i & 0x3FF);
+    chunk3 = ((chunk3 + 1) % 10) + (((((chunk3 % 100) / 10) + 1) % 10) * 10) + ((((chunk3 / 100) + 1) % 10) * 100);
+
+    sprintf(buffer, "%03i%i%i", chunk1, chunk2, chunk3);
+    result.append(buffer);
+
+    return result;
+}
+
+std::string min2Decode(unsigned char input[1]) {
+    char buffer[2];
+    int32_t i, chunk;
+    std::string str, result;
+
+    str = bytesToHex(input, 2, false);
+    i = strtoul(str.c_str(), nullptr, 16);
+
+    chunk = ((i + 1) % 10) + (((((i % 100) / 10) + 1) % 10) * 10) + ((((i / 100) + 1) % 10) * 100);
+
+    sprintf(buffer, "%03i", chunk);
+    result.append(buffer);
+
+    return result;
+}
+
+std::string sidDecode(unsigned char input[1]) {
+    char buffer[4];
+    std::string str, result;
+    uint16_t i;
+
+    str = bytesToHex(input, 2, false);
+    i = std::strtoul(str.c_str(), nullptr, 16);
+
+    sprintf(buffer, "%5i", i);
+    result.append(buffer);
+
+    return result;
+}
+
+long HexToBytes(std::string input) {
+    return std::strtoul(input.c_str(), nullptr, 16);
+}
--- src/util/convert.h
+++ src/util/convert.h
@@ -19,5 +19,9 @@
 
 std::string bytesToHex(unsigned char* input, int size, bool byteswap);
 std::string hexToString(char *input, int length);
+std::string min1Decode(unsigned char input[3]);
+std::string min2Decode(unsigned char input[1]);
+std::string sidDecode(unsigned char input[1]);
+long HexToBytes(std::string input);
 
 #endif
