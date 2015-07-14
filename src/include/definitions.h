ba101c1dc150c20642af723326a2ee3a8142de68
Gassan Idriss <ghassani@gmail.com>
2015-07-14 14:40:59 -0700

    fix gcc macro issue, removed fmt from macro arguments

--- src/include/definitions.h
+++ src/include/definitions.h
@@ -29,8 +29,8 @@
 #define DEBUG_ENABLED 0
 #endif
 
-#define LOGD(fmt, ...) do { if (DEBUG_ENABLED) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
-#define LOGI(fmt, ...) do { fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+#define LOGD(...) do { if (DEBUG_ENABLED) fprintf(stderr, __VA_ARGS__); } while (0)
+#define LOGI(...) do { fprintf(stderr, __VA_ARGS__); } while (0)
 
 
 #endif //_INCLUDE_DEFINITIONS_H
