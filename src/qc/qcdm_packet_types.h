98d5d6a386c7e3cf7ee934834c658bd5b48f5c3e
Matteson Raab <mraabhimself@gmail.com>
2015-07-15 07:28:48 -0700

    Update QCDM packet types in attempt to fix VS2013 warnings

--- src/qc/qcdm_packet_types.h
+++ src/qc/qcdm_packet_types.h
@@ -36,45 +36,50 @@ PACKED(typedef struct qcdm_spc_rx_t{
     uint8_t status;
 }) qcdm_spc_rx_t;
 
-PACKED(typedef struct{
+PACKED(typedef struct qcdm_16pw_tx_t{
     uint8_t command = DIAG_PASSWORD_F;
     uint8_t password[8];
 }) qcdm_16pw_tx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_16pw_rx_t{
     uint8_t command;
     uint8_t status;
 }) qcdm_16pw_rx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_tx_t{
     uint8_t cmd;
     uint16_t nvItem;
     uint8_t data[DIAG_NV_ITEM_SIZE] = { 0x0 };
 }) qcdm_nv_tx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_alt_tx_t{
     uint8_t padding = 0x0;
     uint8_t data[DIAG_NV_ITEM_SIZE - 1] = { 0x0 };
 }) qcdm_nv_alt_tx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_alt2_tx_t{
+    uint16_t padding = 0x0;
+    uint8_t data[DIAG_NV_ITEM_SIZE - 2] = { 0x0 };
+}) qcdm_nv_alt2_tx_t;
+
+PACKED(typedef struct qcdm_nv_raw_tx_t{
     uint8_t data[DIAG_NV_ITEM_SIZE - 3] = { 0x0 };
 }) qcdm_nv_raw_tx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_rx_t{
     uint8_t cmd;
     uint16_t nvItem;
     uint8_t data[DIAG_NV_ITEM_SIZE];
 }) qcdm_nv_rx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_alt_rx_t{
     uint8_t cmd;
     uint16_t nvItem;
     uint8_t padding;
     uint8_t data[DIAG_NV_ITEM_SIZE - 1];
 }) qcdm_nv_alt_rx_t;
 
-PACKED(typedef struct {
+PACKED(typedef struct qcdm_nv_alt2_rx_t{
     uint8_t cmd;
     uint16_t nvItem;
     uint8_t padding[2];
